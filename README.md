10ab2368d3dfdc5e0955d90fa07039e339a95132
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2023-01-12 15:09:43 +1100

    dependencies added to align with forked repo

--- README.md
+++ README.md
@@ -71,3 +71,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 
 ## Resources
 https://react-bootstrap.netlify.app/components/navbar/
+
+https://create-react-app.dev/docs/deployment/#github-pages
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,65 +1,66 @@
 import { useState, useEffect } from "react";
-import { NavBar, Container } from "react-bootstrap";
-
-// import images here as objects so source link doesnt need to be used below
-
-import logo from '../assets/img/daisy.png';
-import navIcon1 from '../assets/img/daisy.png';
-import navIcon2 from '../assets/img/daisy.png';
-import navIcon3 from '../assets/img/daisy.png';
-
-
-// create functional component
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
 
-    // useEffect is used here for when user is scrolling on homepage to detect where they are for hover effect
-    useEffect (() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        }
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-        window.addEventListener("scroll", onScroll);
-
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
-     
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     }
 
-    return (
-        <Navbar expand="lg" className={scolled ? "scrolled": ""}>
-      <Container>
-        <Navbar.Brand href="#home">
-            <img src={logo} alt="logo" />
-            </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-        <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
+    window.addEventListener("scroll", onScroll);
 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active nabar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active nabar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active nabar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-          </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              <div className="social-icon">
                 <a href="#"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
-            </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    )
+              </div>
+              <HashLink to='#connect'>
+                <button className="vvd"><span>Let’s Connect</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
 }
