23ebc957a737d3d67762c26a1db11bf62ddada63
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2023-01-19 14:35:15 +1100

    Updates

---
+++ precache-manifest.03c7c403c5c11e9d6a65890821aa111b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "7499deea238e5f849b97",
+    "url": "/sams-react-portfolio/static/css/main.f378804d.chunk.css"
+  },
+  {
+    "revision": "7499deea238e5f849b97",
+    "url": "/sams-react-portfolio/static/js/main.72039cef.chunk.js"
+  },
+  {
+    "revision": "ea1374c9a4662450cea7",
+    "url": "/sams-react-portfolio/static/js/runtime~main.6786c92f.js"
+  },
+  {
+    "revision": "beed9e8a0b2aa22524e3",
+    "url": "/sams-react-portfolio/static/css/2.24b39bf4.chunk.css"
+  },
+  {
+    "revision": "beed9e8a0b2aa22524e3",
+    "url": "/sams-react-portfolio/static/js/2.f50211e0.chunk.js"
+  },
+  {
+    "revision": "8c3bd72e3d980165ed85",
+    "url": "/sams-react-portfolio/static/js/3.7701c3fb.chunk.js"
+  },
+  {
+    "revision": "46be8d622b11376f242a0db2d80a4bb1",
+    "url": "/sams-react-portfolio/static/media/project-img1.46be8d62.png"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/sams-react-portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/sams-react-portfolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/sams-react-portfolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "c8cacdc9b30c87cb867e9f23b6a4ba07",
+    "url": "/sams-react-portfolio/static/media/homepage-hero.c8cacdc9.svg"
+  },
+  {
+    "revision": "36ade2903b25d57d494f39731aa94b25",
+    "url": "/sams-react-portfolio/static/media/color-sharp2.36ade290.png"
+  },
+  {
+    "revision": "999e859fa71e0517bbdd34d6d20efdfc",
+    "url": "/sams-react-portfolio/static/media/contact-img.999e859f.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sams-react-portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/sams-react-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/sams-react-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/sams-react-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "836443717a99b1c421be32cd9f41ad02",
+    "url": "/sams-react-portfolio/static/media/footer-bg.83644371.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/sams-react-portfolio/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/sams-react-portfolio/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/sams-react-portfolio/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/sams-react-portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/sams-react-portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/sams-react-portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/sams-react-portfolio/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/sams-react-portfolio/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "016fd50068b42921a27bcb7def21ff8d",
+    "url": "/sams-react-portfolio/static/media/color-sharp.016fd500.png"
+  },
+  {
+    "revision": "d088d93cf0b893d29baf0993ef52c663",
+    "url": "/sams-react-portfolio/static/media/logo.d088d93c.svg"
+  },
+  {
+    "revision": "defbe69bc10d52a676173e285755893c",
+    "url": "/sams-react-portfolio/index.html"
+  }
+];
